7fc1740a41b017c19b28ab1535ad8918c6063990
Vadim Remezov <aspowersan@protonmail.com>
2024-04-17 22:29:21 +0300

    feature(RT-40): add empty user screen

---
+++ rentool/lib/features/user/view/user_screen.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class UserScreen extends StatelessWidget {
+  const UserScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
