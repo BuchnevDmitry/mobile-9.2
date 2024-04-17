378fae5eab06525c98be1d779e80ed21c8b7a76b
Vadim Remezov <aspowersan@protonmail.com>
2024-04-17 22:28:47 +0300

    feature(RT-40): add empty shop screen

---
+++ rentool/lib/features/shop/view/shop_screen.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class ShopScreen extends StatelessWidget {
+  const ShopScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
