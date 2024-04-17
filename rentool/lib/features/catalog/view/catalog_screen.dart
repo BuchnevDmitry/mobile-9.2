897c487b68e6e83b6ce5937b752ca13b897b2366
Vadim Remezov <aspowersan@protonmail.com>
2024-04-17 22:39:11 +0300

    feature(RT-40): add empty catalog screen

---
+++ rentool/lib/features/catalog/view/catalog_screen.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class CatalogScreen extends StatelessWidget {
+  const CatalogScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
