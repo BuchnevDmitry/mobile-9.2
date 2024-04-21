ee25db3d257dbc9962646d13fe22b31bc6309173
Vadim Remezov <aspowersan@protonmail.com>
2024-04-22 02:45:35 +0300

    feature(RT-45): add bloc logic for ads_feed screen

---
+++ rentool/lib/features/ads_feed/bloc/ads_feed_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+import 'package:rentool/repositories/api/api.dart';
+import 'package:rentool/repositories/models/models.dart';
+
+part 'ads_feed_event.dart';
+part 'ads_feed_state.dart';
+
+class AdsFeedBloc extends Bloc<AdsFeedEvent, AdsFeedState> {
+  AdsFeedBloc({required this.toolApiClient}) : super(AdsFeedInitialState()) {
+    on<AdsFeedLoadEvent>(_onLoad);
+  }
+
+  Future<void> _onLoad(
+    AdsFeedLoadEvent event,
+    Emitter<AdsFeedState> emit,
+  ) async {
+    try {
+      if (state is! AdsFeedLoadedState) {
+        emit(AdsFeedLoadingState());
+      }
+      final tools = await toolApiClient.getTools();
+      emit(AdsFeedLoadedState(tools: tools));
+    } catch (error) {
+      emit(AdsFeedLoadingFailureState(error: error));
+    } finally {
+      event.completer?.complete();
+    }
+  }
+
+  final ToolsApiClient toolApiClient;
+}
