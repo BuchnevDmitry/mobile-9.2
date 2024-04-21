bf1e86db9199df2ba6bdfc83d2d448805b7d8840
Vadim Remezov <aspowersan@protonmail.com>
2024-04-22 02:39:37 +0300

    chore(RT-45): update class for use model

--- rentool/lib/common/widgets/tool_card.dart
+++ rentool/lib/common/widgets/tool_card.dart
@@ -1,78 +1,83 @@
 import 'package:flutter/material.dart';
-import 'base_container.dart';
+import 'package:rentool/repositories/models/models.dart';
+
+import 'base_round_container.dart';
 
 class ToolCard extends StatelessWidget {
-  ToolCard({super.key, required this.imageUrl});
+  const ToolCard({super.key, required this.tool});
 
-  String imageUrl;
+  final Tool tool;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return BaseContainer(
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.25),
-              spreadRadius: 4,
-              blurRadius: 10,
-              offset: const Offset(0, 5),
-            )
-          ]),
-      child: Column(
-        children: <Widget>[
-          BaseContainer(
-            child: Stack(
-              children: <Widget>[
-                GestureDetector(
-                  child: BaseContainer(
+    return GestureDetector(
+      onTap: () {},
+      child: BaseRoundContainer(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.25),
+                spreadRadius: 4,
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              )
+            ]),
+        child: Column(
+          children: <Widget>[
+            BaseRoundContainer(
+              child: Stack(
+                children: <Widget>[
+                  BaseRoundContainer(
+                    width: 160,
+                    height: 170,
                     child: ClipRect(
-                      child: Image.asset(imageUrl),
+                      child: Image.network(tool.imageUrl),
                     ),
                   ),
-                ),
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.favorite_outline),
+                  Positioned(
+                    top: 0,
+                    right: 0,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.favorite_outline),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Шуруповёрт\nMakito',
-                  style: theme.textTheme.titleSmall,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  'DF457DWEX9',
-                  style: theme.textTheme.bodySmall,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                  textAlign: TextAlign.center,
-                ),
-                Text(
-                  '300 р/час',
-                  style: theme.textTheme.titleSmall,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 1,
-                  textAlign: TextAlign.center,
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    '${tool.category.name}\n${tool.brand.name}',
+                    style: theme.textTheme.titleSmall,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    tool.model,
+                    style: theme.textTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    '${tool.priceHour} р/час',
+                    style: theme.textTheme.titleSmall,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
