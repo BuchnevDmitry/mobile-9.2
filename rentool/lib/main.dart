4681ef181250ec8ffbddd17ff065992bff4e62e1
Vadim Remezov <aspowersan@protonmail.com>
2024-04-29 00:27:13 +0300

    feature(RT-50): add locale dependencie

--- rentool/lib/main.dart
+++ rentool/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:rentool/rentool_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // dotenv init
   await dotenv.load(fileName: '.env');
 
-  runApp(const RenToolApp());
+  initializeDateFormatting('ru_RU', null)
+      .then((_) => runApp(const RenToolApp()));
 }
