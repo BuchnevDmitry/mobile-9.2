3688320c5502d3be48d999762fd5f598ba28e9af
Vadim Remezov <aspowersan@protonmail.com>
2024-04-30 03:14:49 +0300

    feature(RT-55): add configurate for YandexMap

--- rentool/android/app/src/main/kotlin/ru/vsu/cs/rentool/MainActivity.kt
+++ rentool/android/app/src/main/kotlin/ru/vsu/cs/rentool/MainActivity.kt
@@ -1,5 +1,12 @@
 package ru.vsu.cs.rentool
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
+import io.flutter.embedding.engine.FlutterEngine
+import com.yandex.mapkit.MapKitFactory
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        MapKitFactory.setApiKey("API_KEY")
+        super.configureFlutterEngine(flutterEngine)
+  }
+}
