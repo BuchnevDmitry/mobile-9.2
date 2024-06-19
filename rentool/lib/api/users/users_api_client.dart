874f8526a4149b235ef111c475ee74623ba1207a
Vadim Remezov <aspowersan@protonmail.com>
2024-06-19 17:17:47 +0300

    feature(RT-82): add changeUserPassword and rework getUserProfile

--- rentool/lib/api/users/users_api_client.dart
+++ rentool/lib/api/users/users_api_client.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:rentool/api/api.dart';
-import 'package:rentool/api/models/models.dart';
 import 'package:retrofit/http.dart';
 import 'package:retrofit/retrofit.dart';
 
@@ -21,9 +20,13 @@ abstract class UsersApiClient {
   @POST('/api/v1/users/')
   Future<User> registerUser(@Body() User user);
 
-  @GET('/api/v1/users/{id}')
-  Future<User> getUserById(
-    @Path('id') String id,
+  @PATCH('/api/v1/users/change-password')
+  Future<User> changeUserPassword(
     @Header('Authorization') String token,
-  );
+    @Body() Password password, {
+    @Header('Content-Type') String type = 'application/json',
+  });
+
+  @GET('/api/v1/users/profile')
+  Future<User> getUserProfile(@Header('Authorization') String token);
 }
